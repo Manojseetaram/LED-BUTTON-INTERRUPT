8e43a5389cbb9b21ecd36220d6242b2099e328b2
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-07-03 16:35:35 +0530

    create the proejct and set the env for linked script led btn interrupt

--- led-btn/src/main.rs
+++ led-btn/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![allow(clippy::empty_loop)]
+use core::panic:: PanicInfo;
+
+mod startup_stm32f401;
+//global array 
+static mut SCORES_GLOBAL  : [i32; 5 ] = [1 , 2, 3, 4, 5];
+const _NUMBERS  : [i32 ; 5 ] = [1 , 2,3 ,4,5];
+static mut BUFFER : [u8 ; 1024] = [0 ;  1024];
+
+#[unsafe(no_mangle)]
+
+fn main(){
+let mut _total_score = 0 ;
+
+unsafe  {
+for score in SCORES_GLOBAL {
+    _total_score += score;  
+}
+ }
+ unsafe {
+    BUFFER[0] = 100;
+ }
+   loop {
+
+    }
+}
+#[panic_handler]
+fn panic_handler(_info : &PanicInfo)-> ! {
+    loop {
+        
+    }
 }
