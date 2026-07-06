cc34e1f44d77260bc472823e303d95736c98e8b7
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-07-06 19:07:45 +0530

    handle the panic useing panic-halt crate

--- led_toggle_systick/src/main.rs
+++ led_toggle_systick/src/main.rs
@@ -1,13 +1,20 @@
 #![no_std]
 #![no_main]
   
-use cortex_m_rt::entry;
+use cortex_m_rt::{entry, exception};
+use panic_halt as _;
+
 #[entry]
 
   fn main()-> !{
-     
+       
 loop {
     
 }
 
+  }
+
+  #[exception]
+  fn SysTick (){
+    
   }
